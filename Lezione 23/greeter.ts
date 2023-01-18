696fe16b97c48e006ecac9f58770fb27e1ab35bf
Gabriele-Barbone <119305248+Gabriele-Barbone@users.noreply.github.com>
2023-01-18 10:07:50 +0100

    finale

---
+++ Lezione 23/greeter.ts
@@ -0,0 +1,29 @@
+/**
+ * Person class
+ */
+class Person {
+    constructor(firstName: string, lastName: string, city: string = "") {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.city = city;
+    }
+    firstName: string;
+    lastName: string;
+    city: string | null;
+  }
+  
+  /**
+   *
+   * @param person
+   * @returns
+   */
+  function greeter(person: Person) {
+    console.log("dati ricevuti:" + person.firstName + " " + person.lastName);
+    const divInfo = document.getElementById("peopleData");
+  
+    if (divInfo !== null)
+    {
+      divInfo.innerHTML =
+      "<p> <b>" + person.firstName + " " + person.lastName + "</b> vieni da "+person.city+"</p>";
+    }
+  }
