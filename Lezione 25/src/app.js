2e524d90c30485c0037168d3b6bf2f2f21f9b208
Gabriele-Barbone <119305248+Gabriele-Barbone@users.noreply.github.com>
2023-01-24 09:41:08 +0100

    we

---
+++ Lezione 25/src/app.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import HelloWorld from "./components/HelloWorld";
+//import Orologio from "./components/Orologio";
+import BottoneContatore from "./components/BottoneContatore";
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld name="mondo"></HelloWorld>
+      <BottoneContatore></BottoneContatore>
+    </div>
+  );
+}
+~```````
+export default App;
